773f855ff870eec4c73b3030300a558e03d83f56
Lois Tonnet <tonnetlois@gmail.com>
2017-11-22 14:46:15 +0100

    Added special weapon types

--- src/main/java/fr/univtln/m2dapm/boardgame/business/bridgedices/SpecialWeaponType.java
+++ src/main/java/fr/univtln/m2dapm/boardgame/business/bridgedices/SpecialWeaponType.java
@@ -1,6 +1,11 @@
 package fr.univtln.m2dapm.boardgame.business.bridgedices;
 
-public class SpecialWeaponType {
+public enum SpecialWeaponType {
 
-
+    VORTEX_MISSILES,
+    PROTON_RAY,
+    PULLING_RAY,
+    NANO_REPARATOR,
+    MAGNETIC_BOMB,
+    DRONES;
 }
