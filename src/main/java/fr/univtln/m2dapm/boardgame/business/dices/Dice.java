317eff5b6e768fc62694be3cc9c72681f20b0480
Maxime-Temmerier <maxime.temmerier@etud.univ-tln.fr>
2018-01-19 18:22:41 +0100

    Updated Dice

--- src/main/java/fr/univtln/m2dapm/boardgame/business/dices/Dice.java
+++ src/main/java/fr/univtln/m2dapm/boardgame/business/dices/Dice.java
@@ -11,17 +11,15 @@ import java.util.Random;
 
 /**
  * This class represents the concept of a dice that contains a list of sides and can be rolled.
- * @param <T> The type of sides, in case the dice doesn't have integer values on it.
  */
 
 @Entity
-public class Dice<T>  implements Serializable{
+public class Dice  implements Serializable{
 
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     int id;
 
-    protected List<T> sides;
     protected int nbSides;
 
 
@@ -29,8 +27,8 @@ public class Dice<T>  implements Serializable{
      * Rolls the dice once
      * @return a random value from "sides"
      */
-    public T roll() {
-        return sides.get(new Random().nextInt(nbSides));
+    public int roll() {
+        return new Random().nextInt(nbSides);
     }
 
 
@@ -39,8 +37,8 @@ public class Dice<T>  implements Serializable{
      * @param nbRolls the amount of times to roll the dice
      * @return a list of random values from "sides"
      */
-    public List<T> roll(int nbRolls) {
-        List<T> results = new ArrayList<>();
+    public List roll(int nbRolls) {
+        List results = new ArrayList<>();
         for(int i = 0; i < nbRolls; i++)
             results.add(roll());
         return results;
@@ -51,8 +49,7 @@ public class Dice<T>  implements Serializable{
      * @param sides : The list containing all of the dice's sides
      * @param nbSides : The amount of sides
      */
-    public Dice(List<T> sides, int nbSides) {
-        this.sides = sides;
+    public Dice(List sides, int nbSides) {
         this.nbSides = nbSides;
     }
 
@@ -61,7 +58,6 @@ public class Dice<T>  implements Serializable{
      * @param nbSides : The amount of sides
      */
     public Dice(int nbSides) {
-        this.sides = new ArrayList<>();
         this.nbSides = nbSides;
     }
 
@@ -69,18 +65,9 @@ public class Dice<T>  implements Serializable{
      * Constructor
      */
     public Dice() {
-        this.sides = new ArrayList<>();
         this.nbSides = 0;
     }
 
-
-    public List<T> getSides() {
-        return sides;
-    }
-
-    public void setSides(List<T> sides) {
-        this.sides = sides;
-    }
 
     public int getNbSides() {
         return nbSides;
@@ -94,28 +81,7 @@ public class Dice<T>  implements Serializable{
     public String toString() {
         return "Dice{" +
                 "id=" + id +
-                ", sides=" + sides +
                 ", nbSides=" + nbSides +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Dice<?> dice = (Dice<?>) o;
-
-        if (id != dice.id) return false;
-        if (nbSides != dice.nbSides) return false;
-        return sides != null ? sides.equals(dice.sides) : dice.sides == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (sides != null ? sides.hashCode() : 0);
-        result = 31 * result + nbSides;
-        return result;
     }
 }
